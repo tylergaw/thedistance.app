0c02f4bbce902c1a1dbc02f876d8bf74ef529a6b
Tyler Gaw <me@tylergaw.com>
2026-04-19 17:07:59 -0400

    Adds basic map display with maplibre+openfreemap tiles

--- web/eleventy.config.js
+++ web/eleventy.config.js
@@ -9,6 +9,10 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/**/*.css");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/maplibre-gl/dist/maplibre-gl.js": "vendor/maplibre-gl.js",
+    "node_modules/maplibre-gl/dist/maplibre-gl.css": "vendor/maplibre-gl.css",
+  });
 
   eleventyConfig.setServerOptions({
     domDiff: false,
